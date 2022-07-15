c43602bc65c81353d7a551d5bb8f530b38bb221b
Marwa <76475094+ghrairi123@users.noreply.github.com>
2022-07-15 10:49:38 +0100

    seconde commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Store from "./Redux/Store";
 import HomePrincipale from "./Components/VisitorDash/HomePage";
+import error404 from "./Components/HEAD-FOOT-ERR/Error404";
+import Header from "./Components/HEAD-FOOT-ERR/Header";
+import Footer from "./Components/HEAD-FOOT-ERR/Footer";
 function App() {
   return (
     <Provider store={Store}>
       <Router>
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePrincipale} />
+          <Route component={error404} />
         </Switch>
+        <Footer />
       </Router>
     </Provider>
   );
